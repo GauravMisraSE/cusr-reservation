b5a42e38ab9cd40fdb0766dd1ea14ef4b650939c
poojithaamin <poojaamin87@gmail.com>
2017-12-18 01:16:00 -0800

    Add files via upload

---
+++ CMPE275Project/src/main/java/com/cmpe275/cusr/service/TicketService.java
@@ -0,0 +1,13 @@
+package com.cmpe275.cusr.service;
+
+import java.util.List;
+
+import com.cmpe275.cusr.model.Ticket;
+
+public interface TicketService {
+
+
+	Long bookTicket(Ticket ticket);
+	
+
+}
